4d44ef647999f741712dda8b9148dfa8452168ab
François <francois.duguay-giguere.1@ulaval.ca>
2019-04-03 10:52:15 -0400

    / path

--- src/main/java/Api.java
+++ src/main/java/Api.java
@@ -2,6 +2,7 @@ import static spark.Spark.*;
 
 public class Api {
     public static void main(String[] args) {
+        get("/", (req, res) -> "Api FleaMarket");
         get("/hello", (req, res) -> "Hello World");
     }
 
